4c47f95b9d9db7c4fa618eac1a55efb82008af6a
BananaIguanas <nathanj238@gmail.com>
2019-10-29 02:47:17 +1100

    Cleaned up some old comments.

--- instrument.py
+++ instrument.py
@@ -1,5 +1,5 @@
 # IMPORTANT: You need to install the playSound module with "pip3 install playsound" (On Linux terminal) for this to work.
-# To use example: run in shell with "python3 playingSound.py".
+# To use example: Run "playingSound.py"
 
 from playsound import playsound
 import numpy as np
