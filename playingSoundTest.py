146b84c71706550aaab494decc97d507f518006f
BananaIguanas <nathanj238@gmail.com>
2019-10-29 16:03:43 +1100

    Removed irrelevant machine learning code

--- playingSoundTest.py
+++ playingSoundTest.py
@@ -1,14 +1,5 @@
 from instrument import instrument
 
-a = (clf.predict(temp))[0][0]
-
-if a == 1:
-    trombone.action(1)
-elif a == 0:
-    trombone.action(2)
-
-
-
 trombone = instrument("tromboneSounds.txt")
 
 trombone.action(3)
